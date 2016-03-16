cb2a85b89d6cca0a414d3cccaaefe2586ccee60d
Roman Dvornov <rdvornov@gmail.com>
2016-03-16 17:05:18 +0500

    use AST convertations instead of re-parsing

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var postcss = require('postcss');
-var csso = require('csso');
+var csso = require('csso').compress;
+var postcssToCsso = require('./lib/postcssToCsso.js');
+var cssoToPostcss = require('./lib/cssoToPostcss.js');
+
+function extend(dest, source) {
+    return Object.keys(source).reduce(function(result, key) {
+        dest[key] = source[key];
+        return dest;
+    }, dest || {});
+}
  
 var postcssCsso = postcss.plugin('postcss-csso', function postcssCsso(options) {
-    return function (root) {
-        var minified = csso.minify(root.toString(), options);
-        
-        root.nodes = postcss
-            .parse(minified)
-            .nodes
-            .map(function(node) {
-                node.parent = root;
-                return node;
-            });
+    return function(root, result) {
+        result.root = cssoToPostcss(csso(postcssToCsso(root), extend({
+            outputAst: true
+        }, options || {})));
     }
 });
 
